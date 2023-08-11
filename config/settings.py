2ff27c04e3677d162c2b30ffac475ecf9053b884
Subkhon Amrulloev <amrulloev.suhon@gmail.com>
2023-08-11 16:07:10 +0900

    family spending django rest app

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,9 @@ import logging
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- family_spending/basic_views.py
+++ family_spending/basic_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+import os
 
 def base_view(request):
-    context={"api_hostname_from_django":"http://localhost:8000"}
+    context={"api_hostname_from_django":f"http://{os.environ.get('hostname')}:8000"}
     return render(request,"index.html",context)
