078cb54a4314af4b743ec007a7aa9b0bdaabc988
Subkhon Amrulloev <amrulloev.suhon@gmail.com>
2023-08-12 17:49:33 +0900

    family spending app

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-t1h-+rp2iakm47smo6kf4#=9%483wqzhb3r0nks9&d&w43f^^n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","http://23.100.89.58/"]
 
 
 # Application definition
